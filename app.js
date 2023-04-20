244e59c41408e2fc44ed56e111ae9bcfbcae1ee7
Saad96Raza <razasaad118@gmail.com>
2023-04-20 07:47:25 +0500

    webpack update

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+
+
+app.use(express.static(path.join(__dirname , 'public')))
+
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','pug')
 
